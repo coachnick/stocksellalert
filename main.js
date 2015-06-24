02321c5488dd694715cf88c5033c4f0fc1d335e9
coachnick <coach.nick@gmail.com>
2015-06-25 02:03:16 +0800

    initial the idea

---
+++ main.js
@@ -0,0 +1,25 @@
+/*
+ input stock no and buy price
+ 
+ it need to read config file about period and stop loss level
+ 
+ set alert information
+ for every 15 mins{
+ get stock data 
+ check rules
+ push msg if need
+ update cache
+ }
+ */
+
+function pushMsg(msg){
+	
+}
+
+function getStockInfo(stockNo){
+	
+}
+
+function ruleChecking(){
+	
+}
